3a5f13a9dc9d898e4bf5b2e78a318825876311c6
99blanc <t990421@naver.com>
2025-11-02 17:33:36 +0900

    [Chore] 다크 모드 로직 수정

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,54 +1,52 @@
-function toggleDarkMode() {
+function toggleDarkMode(toggleElement){
     const DARK_CLASS = 'dark';
-    var body = document.querySelector("body");
-    if (body.classList.contains(DARK_CLASS)) {
-        setCookie('theme', 'light');
-        body.classList.remove(DARK_CLASS);
-        giscusTheme = 'noborder_light';
-    } else {
-        setCookie('theme', 'dark');
+    const body = document.querySelector("body");
+    const isChecked = toggleElement.checked;
+    let giscusTheme;
+    if(isChecked){
+        setCookie('theme','dark');
         body.classList.add(DARK_CLASS);
-        giscusTheme = 'noborder_gray';
+        giscusTheme='noborder_gray';
+    }else{
+        setCookie('theme','light');
+        body.classList.remove(DARK_CLASS);
+        giscusTheme='noborder_light';
     }
-    if (typeof setGiscusTheme === 'function') {
+    if(typeof setGiscusTheme==='function'){
         setGiscusTheme(giscusTheme);
     }
 }
-function getCookie(name) {
-    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return v ? v[2] : null;
+function getCookie(name){
+    var v=document.cookie.match('(^|;) ?'+name+'=([^;]*)(;|$)');
+    return v?v[2]:null;
 }
-function setCookie(name, value, days) {
-    var d = new Date;
-    d.setTime(d.getTime() + 24*60*60*1000*days);
-    document.cookie = name + "=" + value + ";path=/;SameSite=strict;expires=" + d.toGMTString();
+function setCookie(name,value,days){
+    var d=new Date;
+    d.setTime(d.getTime()+24*60*60*1000*days);
+    document.cookie=name+"="+value+";path=/;SameSite=strict;expires="+d.toGMTString();
 }
-function deleteCookie(name) { setCookie(name, '', -1); }
-function initializeDarkMode() {
-    const DARK_CLASS = 'dark';
-    var body = document.querySelector("body");
-    var toggles = document.querySelectorAll('.dark-mode-toggle');
-    toggles.forEach(ti => ti.classList.add('no-transition'));
-    body.classList.add(DARK_CLASS);
-    toggles.forEach(ti => ti.checked = true);
-    window.requestAnimationFrame(() => {
-        toggles.forEach(ti => ti.checked = true);
-        toggles.forEach(ti => ti.classList.remove('no-transition'));
-    });
-}
-var userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-var theme = getCookie('theme');
-window.addEventListener('DOMContentLoaded', () => {
-    if ( (theme === null && userPrefersDark) || theme === 'dark') {
-        var checkDarkDone = false;
-        function checkDarkAndInitialize() {
-            if (!checkDarkDone) {
-                initializeDarkMode();
-            }
-            checkDarkDone = true;
-        };
-        if (window.requestAnimationFrame) 
-            window.requestAnimationFrame(checkDarkAndInitialize);
-        checkDarkAndInitialize();
+function deleteCookie(name){setCookie(name,'',-1);}
+var userPrefersDark=window.matchMedia&&window.matchMedia('(prefers-color-scheme: dark)').matches;
+var theme=getCookie('theme');
+window.addEventListener('DOMContentLoaded',()=>{
+    const DARK_CLASS='dark';
+    const body=document.querySelector("body");
+    const toggles=document.querySelectorAll('.dark-mode-toggle');
+    toggles.forEach(ti=>ti.classList.add('no-transition'));
+    if((theme===null&&userPrefersDark)||theme==='dark'){
+        body.classList.add(DARK_CLASS);
+        toggles.forEach(ti=>ti.checked=true);
+        if(typeof setGiscusTheme==='function'){
+            setGiscusTheme('noborder_gray');
+        }
+    }else{
+        body.classList.remove(DARK_CLASS);
+        toggles.forEach(ti=>ti.checked=false);
+        if(typeof setGiscusTheme==='function'){
+            setGiscusTheme('noborder_light');
+        }
     }
+    window.requestAnimationFrame(()=>{
+        toggles.forEach(ti=>ti.classList.remove('no-transition'));
+    });
 });
